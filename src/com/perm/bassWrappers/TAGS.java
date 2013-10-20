55958fce89c74ca9079fe57d4a57d1cbfc2323f6
Doom <doomplaye@gmail.com>
2013-10-20 14:26:14 +0300

    prepare to bass

---
+++ src/com/perm/bassWrappers/TAGS.java
@@ -0,0 +1,13 @@
+package com.perm.bassWrappers;
+
+public class TAGS
+{
+	public static native String TAGS_GetLastErrorDesc();
+	public static native String TAGS_Read(int dwHandle, String fmt);
+	public static native String TAGS_ReadEx(int dwHandle, String fmt, int tagtype);
+	public static native int TAGS_GetVersion();
+	
+    static {
+        System.loadLibrary("tags");
+    }
+}
